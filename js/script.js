4704a122558770a3ed4aebd6d5e2240c0ce3333d
Thienan Nguyen <thienan.nguyen@ousd.us>
2018-10-04 00:57:10 +0000

    Added a image slideshow. organized the navbar and footer. Added flexbox to the about section.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 // JavaScript File
 /*global $*/
+var x = 1;
 
-$('body').click(function(){
+$('.thumbnail').click(function(){
+    x = x + 1;
+    if (x > 4){
+        x = 1;
+    }
+    $("#img1").css("display", "none");
+    $("#img2").css("display", "none");
+    $("#img3").css("display", "none");
+    $("#img4").css("display", "none");
+    $("#img"+x).css("display", "block");
     
+    console.log(x);
 });
 
